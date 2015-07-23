e13faf4b55c0fea94347540120041abf23a5dd8d
Karol Pilch <karoop@Karol-Pilchs-iMac.local>
2015-07-23 22:21:47 +0100

    Initial Commit

---
+++ CppCourse/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppCourse
+//
+//  Created by Karol Pilch on 23/07/2015.
+//  Copyright (c) 2015 Karol Pilch. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
